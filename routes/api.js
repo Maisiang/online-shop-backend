fdcec5cce4457f5a39e0f91290b442cdc5bd0872
maisiang <maisiang001@gmail.com>
2023-02-21 13:19:19 +0800

    修改驗證登入中間件

--- routes/api.js
+++ routes/api.js
@@ -30,58 +30,43 @@ const upload = multer({
   }
 })
 
-
-// 路由級別中間件 - 處理身分驗證 
-router.use(auth = function(request,response,next){
-  try{
-    console.log('\n');
-    // 設定不用身分驗證的路由
-    const notAuthRoute=['/user/logout', '/user/login', '/user/register',
-                        '/product/'];
-    if(notAuthRoute.indexOf(request.path)!=-1){
-      return next();
-    }
-    // 其餘頁面要做身分驗證 - 前端頁面還沒加判斷
-    console.log('身分驗證',request.session.user);
-    if(request.session.user===undefined){
-      console.log('請求失敗：',request.method,request.path);
-      response.send({
-        isLogin:false,
-        message:'操作失敗，請先登入會員！'
-      });
-      return false;
-    }
-    // 當通過身分驗證
+// 檢查用戶是否登入
+const requireLogin = (request,response,next) => {
+  if (request.session.user === undefined ){
+    if(request.path === '/user/loginStatus' && request.method === 'GET')
+      response.send({isLogin:false});
+    else
+      response.status(401).send('Unauthorized');
+  }
+  else{
+    console.log('登入: true')
     next();
   }
-  catch(error){
-    console.log("錯誤："+ error.message);
-  }
-});
+};
 
 // 商品
 router.get('/product', product.getProduct); //取得商品
 
 // 購物車
-router.get('/cart'                , cart.getCart);    // 取得購物車
-router.post('/cart/:product_id'   , cart.addCart);    // 新增購物車商品
-router.delete('/cart/:product_id' , cart.deleteCart); // 移除購物車商品
+router.get('/cart'                , requireLogin , cart.getCart);    // 取得購物車
+router.post('/cart/:product_id'   , requireLogin , cart.addCart);    // 新增購物車商品
+router.delete('/cart/:product_id' , requireLogin , cart.deleteCart); // 移除購物車商品
 
 // 用戶
-router.get('/user'          , user.getUserInfo);                            // 取得用戶資訊
+router.get('/user'          , requireLogin , user.getUserInfo);             // 取得用戶資訊
 router.post('/user/register', parseForm.array(), user.register);            // 新增用戶
-router.put('/user/password' , user.updatePassword);                         // 更新密碼 
-router.put('/user/avatar'   , upload.single("avatar"), avatar.updateAvatar);// 更新頭像
+router.put('/user/password' , requireLogin , user.updatePassword);                         // 更新密碼 
+router.put('/user/avatar'   , requireLogin , upload.single("avatar"), avatar.updateAvatar);// 更新頭像
 
 // 用戶 - Session
-router.post('/user/login'    , user.login);  // 建立Session
-router.post('/user/logout'   , user.logout); // 移除Session
-router.get('/user/isLogin'   , user.isLogin);// 取得Session
+router.post('/user/login'       , user.login);  // 用戶登入
+router.post('/user/logout'      , user.logout); // 用戶登出
+router.get('/user/loginStatus'  , requireLogin  , user.isLogin); // 取得登入狀態
 
 
 // 交易紀錄
-router.get('/transaction'  , transaction.getTransaction);  // 取得交易紀錄
-router.post('/transaction' , transaction.addTransaction);  // 新增交易紀錄
+router.get('/transaction'  , requireLogin , transaction.getTransaction);  // 取得交易紀錄
+router.post('/transaction' , requireLogin , transaction.addTransaction);  // 新增交易紀錄
 
 
 
